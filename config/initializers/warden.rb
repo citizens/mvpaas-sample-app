7ca78034a66d5e3c73daf5b94208b676947f4626
Dylan Montgomery <mail@citizensinspace.com>
2017-01-18 20:13:18 -0500

    created a warden strategy for development

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,7 +1,7 @@
 require 'mvpaas'
 
 Rails.configuration.middleware.use RailsWarden::Manager do |manager|
-  manager.default_strategies :mvpaas
+  manager.default_strategies Rails.env.development? ? :mvpaas_dev : :mvpaas
   manager.failure_app = 'ApplicationController'
 end
 
@@ -13,9 +13,10 @@ class Warden::SessionSerializer
 
   def deserialize(keys)
     klass, id = keys
-    klass.find(:first, :conditions => { :id => id })
+    Object.const_get(klass).find_by(id: id)
   end
 end
+
 
 Warden::Strategies.add(:mvpaas) do
   # If the token is invalid, an exception will be raised.
@@ -32,5 +33,20 @@ Warden::Strategies.add(:mvpaas) do
     success!(user)
   rescue JWT::DecodeError
     fail!
+  end
+end
+
+# A development strategy
+Warden::Strategies.add(:mvpaas_dev) do
+  def authenticate!
+    data = { id: '123', email: 'mail@example.com' }
+    user = User.find_or_initialize_by(external_id: data[:id])
+
+    if user.new_record?
+      user.email = data[:email]
+      user.save
+    end
+
+    success!(user)
   end
 end
