ed098eef21bb5cdbf49bf4426f326760d2af018b
Dylan Montgomery <mail@citizensinspace.com>
2017-01-18 19:03:46 -0500

    auth spec

---
+++ spec/requests/warden_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe 'Warden', type: :request do
+  before { ENV['MVPAAS_SECRET'] = pem.to_s }
+  after  { ENV['MVPAAS_SECRET'] = nil }
+
+  describe "GET #index" do
+    let(:pem) { generate_pem }
+    let(:token) { generate_jwt(pem) }
+    let(:redirect_url) { "#{ENV['MVPAAS_URL']}/sign_in.html" }
+    
+    context 'with a valid token' do
+      it "logs the user in" do
+        get '/dashboard', params: { token: token }
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'with an empty token' do
+      it "redirects to the MVP sign in page" do
+        get '/dashboard', params: { token: nil }
+        expect(response).to redirect_to(redirect_url)
+      end
+    end
+
+    context 'with an invalid token' do
+      it "redirects to the MVP sign in page" do
+        get '/dashboard', params: { token: 'xxx' }
+        expect(response).to redirect_to(redirect_url)
+      end
+    end
+  end
+end
